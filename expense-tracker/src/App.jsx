eeae627f38c3110afc70c9fe9a8c70107e40399e
Ayush Sharma <asayush35@gmail.com>
2025-01-09 19:57:55 +0530

    Adding components

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -2,34 +2,62 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import ExpenseState from './Contexts/ExpenseState';
+import { Alert } from './Components/Alert';
+import { Home } from './Components/Home';
+import About from './Components/About';
+import Login from './Components/Login';
+import Signup from './Components/SignUP';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ExpenseState>
+      <Router>
+        <Navbar />
+        <Alert message="Welcome to my Expense Tracker App" />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </Router>
+    </ExpenseState>
+  /*  <>
+      <ExpenseState>
+        <Router>
+          <Navbar />
+          <Alert message="Welcome to my Expense Tracker App" />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />}>
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+            </Routes>
+          </div>
+        </Router>
+      </ExpenseState>
+    </>*/
+  );
 }
 
-export default App
+export default App;
+
